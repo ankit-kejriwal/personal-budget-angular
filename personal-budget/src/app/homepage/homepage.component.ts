9840fcea86d3a2f42879c001cb3ce9cd567ac71a
ankit <ankitkejriwaal@gmail.com>
2020-10-09 11:18:13 -0400

    removed extra code

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -10,25 +10,6 @@ import { DataService } from '../data.service';
 })
 export class HomepageComponent implements OnInit {
 
-  // public dataSource = {
-  //   datasets: [{
-  //       data: [],
-  //       backgroundColor: [
-  //           '#3e95cd',
-  //           '#8e5ea2',
-  //           '#3cba9f',
-  //           '#e8c3b9',
-  //           '#c45850',
-  //           'rgba(255,221,50,0.2)',
-  //           'rgba(60,186,159,0.2)',
-  //           'rgba(0,0,0,0.2)',
-  //           'rgba(193,46,12,0.2)'
-  //       ],
-  //   }],
-
-  //   // These labels appear in the legend and in the tooltips when hovering different arcs
-  //   labels: []
-  // };
   private svg: d3.Selection<any>;
 
   constructor(private http: HttpClient, private dataService: DataService) { }
